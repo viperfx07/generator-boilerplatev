e350ccbabba4f52fe04df980391cd89d5e10fda3
Victor Tanudjaja <victor@webcoda.com.au>
2017-05-11 13:50:03 +1000

    imagemin fixed

--- generators/app/templates/gulp/imagemin.js
+++ generators/app/templates/gulp/imagemin.js
@@ -5,17 +5,16 @@ import gulpif from 'gulp-if';
 import pngquant from 'imagemin-pngquant';
 
 export default function(gulp, plugins, args, config, taskTarget, browserSync, dirs) {
-  let dest = path.join(taskTarget, dirs.assets, dirs.images.replace(/^_/, ''));
+    let dest = path.join(taskTarget, dirs.assets, dirs.images.replace(/^_/, ''));
 
-  // Imagemin
-  gulp.task('imagemin', () => {
-    return gulp.src(path.join(dirs.source, dirs.images, '**/*.{jpg,jpeg,gif,svg,png}'))
-      .pipe(plugins.changed(dest))
-      .pipe(plugins.imagemin({
-        progressive: true,
-        svgoPlugins: [{removeViewBox: false}],
-        use: [pngquant({speed: 10})]
-      }))
-      .pipe(gulp.dest(dest));
-  });
+    // Imagemin
+    gulp.task('imagemin', () => {
+        return gulp.src(path.join(dirs.source, dirs.images, '**/*.{jpg,jpeg,gif,svg,png}'))
+            .pipe(plugins.changed(dest))
+            .pipe(plugins.imagemin([
+                plugins.imagemin.jpegtran({ progressive: true }),
+                plugins.imagemin.svgo({ plugins: [{ removeViewBox: false }] })
+            ], { use: [pngquant({ speed: 10 })] }))
+            .pipe(gulp.dest(dest));
+    });
 }
