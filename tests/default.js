14b76974c9dee83f40e3e0688613843199bc1547
Victor <viperfx07@gmail.com>
2017-08-05 13:42:04 +1000

    try updated test

--- tests/default.js
+++ tests/default.js
@@ -2,33 +2,47 @@ var helpers = require('yeoman-test');
 var path = require('path');
 var exec = require('child_process').exec;
 var gulp = require('gulp');
-
-var fn = function(error, stdout, stderr){
-    console.log(error);
-    if(!!error) done(error);
-};
+var assert = yeoman.assert;
 
 describe('generator-boilerplatev:app', function(){
-    
-    it('generates and tries to build for production', function(done){
-        this.timeout(300000);
-        helpers
-            .run(path.join(__dirname, '../generators/app'))
-            .withPrompts({
-                cssFramework: 'bootstrap',
-                vue: false,
-                useNotif: true,
-                skipInstall: false
-            }) // Mock the prompt answers
-            .inTmpDir(function(dir){
-                console.log(dir);
-                exec('yarn --production', fn)
+    let hasError = 0;
+    describe('standard build', function(){
+        before(function(done){
+            this.timeout(300000);
+            helpers
+                .run(path.join(__dirname, '../generators/app'))
+                .withPrompts({
+                    cssFramework: 'bootstrap',
+                    vue: false,
+                    useNotif: true,
+                    skipInstall: false
+                }) // Mock the prompt answers
+                .inTmpDir(function(dir){
+                    console.log(dir);
+                    exec('yarn --production', function(error, stdout, stderr){
+                        console.log(error);
+                        if(!!error){
+                            hasError = 1;
+                            done(error);
+                        }
+                    })
                     .on('exit', function(){
-                        exec('gulp --production=dev', fn)
+                        exec('gulp --production=dev', function(error, stdout, stderr){
+                            console.log(error);
+                            if(!!error){
+                                hasError = 1;
+                                done(error);
+                            }
+                        })
                         .on('exit', function(){
                             done();
                         })
                     });
-            })
-    });
+                })
+        
+        })
+        it('generates and tries to build for production', function(done){
+            assert(!!hasError);
+        });
+    })
 });
