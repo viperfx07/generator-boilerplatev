e900246712558b5e47f1ab00295c9c5d6a1dbdc4
Victor <victor@webcoda.com.au>
2019-04-12 14:38:10 +1000

    fixed imagemin quality settings that causes an error
    https://www.npmjs.com/package/imagemin-pngquant#quality

--- generators/app/templates/gulp/imagemin.js
+++ generators/app/templates/gulp/imagemin.js
@@ -13,7 +13,7 @@ export default function(gulp, plugins, args, config, taskTarget, browserSync, di
         return gulp.src(path.join(dirs.source, dirs.images, '**/*.{jpg,jpeg,gif,svg,png}'))
             .pipe(plugins.imagemin([
                 plugins.imagemin.svgo({ plugins: [{ removeViewBox: false }] }),
-                pngquant({speed: 1, quality:'85'}),
+                pngquant({speed: 1, quality: [.85, .85]}),
                 jpegoptim({progressive: true, max: 85}),
             ], { verbose: true }))
             .pipe(gulp.dest(dest));
